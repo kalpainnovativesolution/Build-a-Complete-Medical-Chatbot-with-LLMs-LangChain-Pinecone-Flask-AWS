89bb2215716905c9c4e7ae648f6f444b6f2323fc
kalpa <kalpa@LT304.corp.promptnet.in>
2026-04-30 16:57:39 +0530

    Added requirement and setup file again

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nitin Ravat",
+    author_email="nitinravat2000@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
